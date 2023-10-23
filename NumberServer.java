a38240669305d18decc9cb2e7535aaa6741a7324
claudiaaling <147002953+claudiaaling@users.noreply.github.com>
2023-10-22 20:54:44 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -2,36 +2,33 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    int num = 0;
-
+    String message = "";
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+        if (url.getPath().equals("/add-message")) {
+            String query = url.getQuery();
+            if (query != null && query.startsWith("s=")) {
+                String newMessage = query.substring(2);
+                if (message.isEmpty()) {
+                message += "1. " + newMessage;
+                }         
+                else {
+                String[] lines = message.split("\n");
+                 int count = lines.length + 1;
+                 message += "\n" + count + ". " + newMessage;
                 }
+                return message;
             }
-            return "404 Not Found!";
         }
+        return "404 Not Found!";
     }
 }
 
-class NumberServer {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
-
         int port = Integer.parseInt(args[0]);
 
         Server.start(port, new Handler());
